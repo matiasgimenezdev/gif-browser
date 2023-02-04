acd0d778f69217860da3ebc224d7074f4d1e5c5c
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-04 10:28:27 -0300

    Modifies the message showed when there is no elements

--- src/GifBrowserApp.jsx
+++ src/GifBrowserApp.jsx
@@ -21,7 +21,7 @@ function GifExpertApp() {
 				})
 			) : (
 				<p className='noSearchMessage'>
-					Make a Search{' '}
+					Make a Search...
 					<img
 						src='https://images.unsplash.com/photo-1487260211189-670c54da558d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
 						alt='empty'
