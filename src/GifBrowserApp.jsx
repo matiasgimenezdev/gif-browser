7d5c9b6e2bf1010c7c40c4371f3c24b3c7787bd2
matiasgimenezdev <matiasgimenez.dev@gmail.com>
2023-02-04 10:21:25 -0300

    Adds message when there is no elements

--- src/GifBrowserApp.jsx
+++ src/GifBrowserApp.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import { Form, GifGrid } from './components';
 
 function GifExpertApp() {
-	const [categories, setCategories] = useState(['Valorant']);
+	const [categories, setCategories] = useState([]);
 
 	const handleAdd = (category) => {
 		if (!categories.includes(category)) {
@@ -11,14 +11,24 @@ function GifExpertApp() {
 	};
 
 	return (
-		<>
-			<h1>GIF Browser</h1>
+		<div className='app'>
+			<h1 className='heading'>GIF Browser</h1>
 			<Form handleAdd={handleAdd} />
 
-			{categories.map((category) => {
-				return <GifGrid key={category} category={category} />;
-			})}
-		</>
+			{categories.length > 0 ? (
+				categories.map((category) => {
+					return <GifGrid key={category} category={category} />;
+				})
+			) : (
+				<p className='noSearchMessage'>
+					Make a Search{' '}
+					<img
+						src='https://images.unsplash.com/photo-1487260211189-670c54da558d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+						alt='empty'
+					/>
+				</p>
+			)}
+		</div>
 	);
 }
 
